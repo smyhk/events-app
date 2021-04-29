bff47cd49b3dd0b849ecd523231dc8031b5d0432
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-04-29 12:23:02 -0700

    Add styles to event list and item

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,9 +1,11 @@
 import EventItem from "./event-item";
 
+import classes from "./event-list.module.css";
+
 export default function EventList(props) {
   const { events } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem
           key={event.id}
