2f6865f1c10bc22c478b114ac23885486fca5223
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-04-29 12:10:07 -0700

    Implement event list and item

---
+++ components/events/event-item.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  // Reformat date and address
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(",", "\n");
+  //
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
---
+++ components/events/event-list.js
@@ -0,0 +1,19 @@
+import EventItem from "./event-item";
+
+export default function EventList(props) {
+  const { events } = props;
+  return (
+    <ul>
+      {events.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
