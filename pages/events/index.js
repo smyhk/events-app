90ce5ba35e5c4b8f5d713a6d2ad780738b855544
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-04-30 12:22:40 -0700

    Define events search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,14 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventsSearch />
+      <EventList events={events} />
     </div>
   );
 }
